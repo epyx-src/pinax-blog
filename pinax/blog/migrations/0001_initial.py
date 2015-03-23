7b9d8f0159ad63787b92994b22c5b8eb59725e3b
Yann Savary <yann.savary@epyx.ch>
2015-03-23 15:09:42 +0100

    Added old south migration

--- pinax/blog/migrations/0001_initial.py
+++ pinax/blog/migrations/0001_initial.py
@@ -1,101 +1,171 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
 
-from django.db import models, migrations
-from django.utils import timezone
-from django.conf import settings
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Post'
+        db.create_table(u'blog_post', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('section', self.gf('django.db.models.fields.IntegerField')()),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=90)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(related_name='posts', to=orm['core.User'])),
+            ('markup', self.gf('django.db.models.fields.CharField')(max_length=25)),
+            ('teaser_html', self.gf('django.db.models.fields.TextField')()),
+            ('content_html', self.gf('django.db.models.fields.TextField')()),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('primary_image', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='+', null=True, to=orm['blog.Image'])),
+            ('tweet_text', self.gf('django.db.models.fields.CharField')(max_length=140)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('updated', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('published', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('state', self.gf('django.db.models.fields.IntegerField')(default=1)),
+            ('secret_key', self.gf('django.db.models.fields.CharField')(unique=True, max_length=8, blank=True)),
+            ('view_count', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal(u'blog', ['Post'])
+
+        # Adding model 'Revision'
+        db.create_table(u'blog_revision', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('post', self.gf('django.db.models.fields.related.ForeignKey')(related_name='revisions', to=orm['blog.Post'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=90)),
+            ('teaser', self.gf('django.db.models.fields.TextField')()),
+            ('content', self.gf('django.db.models.fields.TextField')()),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(related_name='revisions', to=orm['core.User'])),
+            ('updated', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('published', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('view_count', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal(u'blog', ['Revision'])
+
+        # Adding model 'Image'
+        db.create_table(u'blog_image', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('post', self.gf('django.db.models.fields.related.ForeignKey')(related_name='images', to=orm['blog.Post'])),
+            ('image_path', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('url', self.gf('django.db.models.fields.CharField')(max_length=150, blank=True)),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+        ))
+        db.send_create_signal(u'blog', ['Image'])
+
+        # Adding model 'FeedHit'
+        db.create_table(u'blog_feedhit', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('request_data', self.gf('django.db.models.fields.TextField')()),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+        ))
+        db.send_create_signal(u'blog', ['FeedHit'])
+
+        # Adding model 'ReviewComment'
+        db.create_table(u'blog_reviewcomment', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('post', self.gf('django.db.models.fields.related.ForeignKey')(related_name='review_comments', to=orm['blog.Post'])),
+            ('review_text', self.gf('django.db.models.fields.TextField')()),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('addressed', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'blog', ['ReviewComment'])
 
 
-class Migration(migrations.Migration):
+    def backwards(self, orm):
+        # Deleting model 'Post'
+        db.delete_table(u'blog_post')
 
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
+        # Deleting model 'Revision'
+        db.delete_table(u'blog_revision')
 
-    operations = [
-        migrations.CreateModel(
-            name='FeedHit',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('request_data', models.TextField()),
-                ('created', models.DateTimeField(default=timezone.now)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Image',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('image_path', models.ImageField(upload_to=b'images/%Y/%m/%d')),
-                ('url', models.CharField(max_length=150, blank=True)),
-                ('timestamp', models.DateTimeField(default=timezone.now, editable=False)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Post',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('section', models.IntegerField(choices=[(1, 'all'), (2, b'Release Notes')])),
-                ('title', models.CharField(max_length=90)),
-                ('slug', models.SlugField()),
-                ('markup', models.CharField(max_length=25, choices=[('markdown', 'Markdown'), ('creole', 'Creole')])),
-                ('teaser_html', models.TextField(editable=False)),
-                ('content_html', models.TextField(editable=False)),
-                ('description', models.TextField(blank=True)),
-                ('tweet_text', models.CharField(max_length=140, editable=False)),
-                ('created', models.DateTimeField(default=timezone.now, editable=False)),
-                ('updated', models.DateTimeField(null=True, editable=False, blank=True)),
-                ('published', models.DateTimeField(null=True, editable=False, blank=True)),
-                ('secret_key', models.CharField(help_text=b'allows url for sharing unpublished posts to unauthenticated users', unique=True, max_length=8, blank=True)),
-                ('view_count', models.IntegerField(default=0, editable=False)),
-                ('author', models.ForeignKey(related_name='posts', to=settings.AUTH_USER_MODEL)),
-                ('primary_image', models.ForeignKey(related_name='+', blank=True, to='blog.Image', null=True)),
-            ],
-            options={
-                'ordering': ('-published',),
-                'get_latest_by': 'published',
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='ReviewComment',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('review_text', models.TextField()),
-                ('timestamp', models.DateTimeField(default=timezone.now)),
-                ('addressed', models.BooleanField(default=False)),
-                ('post', models.ForeignKey(related_name='review_comments', to='blog.Post')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Revision',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(max_length=90)),
-                ('teaser', models.TextField()),
-                ('content', models.TextField()),
-                ('updated', models.DateTimeField(default=timezone.now)),
-                ('published', models.DateTimeField(null=True, blank=True)),
-                ('view_count', models.IntegerField(default=0, editable=False)),
-                ('author', models.ForeignKey(related_name='revisions', to=settings.AUTH_USER_MODEL)),
-                ('post', models.ForeignKey(related_name='revisions', to='blog.Post')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.AddField(
-            model_name='image',
-            name='post',
-            field=models.ForeignKey(related_name='images', to='blog.Post'),
-            preserve_default=True,
-        ),
-    ]
+        # Deleting model 'Image'
+        db.delete_table(u'blog_image')
+
+        # Deleting model 'FeedHit'
+        db.delete_table(u'blog_feedhit')
+
+        # Deleting model 'ReviewComment'
+        db.delete_table(u'blog_reviewcomment')
+
+
+    models = {
+        u'blog.feedhit': {
+            'Meta': {'object_name': 'FeedHit'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'request_data': ('django.db.models.fields.TextField', [], {})
+        },
+        u'blog.image': {
+            'Meta': {'object_name': 'Image'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image_path': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'post': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'images'", 'to': u"orm['blog.Post']"}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '150', 'blank': 'True'})
+        },
+        u'blog.post': {
+            'Meta': {'ordering': "('-published',)", 'object_name': 'Post'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'posts'", 'to': u"orm['core.User']"}),
+            'content_html': ('django.db.models.fields.TextField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'markup': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'primary_image': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'+'", 'null': 'True', 'to': u"orm['blog.Image']"}),
+            'published': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'secret_key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '8', 'blank': 'True'}),
+            'section': ('django.db.models.fields.IntegerField', [], {}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'state': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'teaser_html': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '90'}),
+            'tweet_text': ('django.db.models.fields.CharField', [], {'max_length': '140'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'view_count': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        u'blog.reviewcomment': {
+            'Meta': {'object_name': 'ReviewComment'},
+            'addressed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'review_comments'", 'to': u"orm['blog.Post']"}),
+            'review_text': ('django.db.models.fields.TextField', [], {}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
+        },
+        u'blog.revision': {
+            'Meta': {'object_name': 'Revision'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'revisions'", 'to': u"orm['core.User']"}),
+            'content': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'revisions'", 'to': u"orm['blog.Post']"}),
+            'published': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'teaser': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '90'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'view_count': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        u'core.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '1023', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_sales': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_service_for_tenant': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'lang': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '5', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'notify_system': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'notify_watching': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'otp_client_secret': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'otp_client_valid': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'use_beta': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'use_two_factor': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['blog']
